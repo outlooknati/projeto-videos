2f6fad8560b278d21076e1a000c3f9f6c1a44436
outlooknati <natisales2107@gmail.com>
2025-10-21 09:40:16 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -71,4 +71,35 @@ app.get('/protected', (req, res) => {
         }catch(err){
             res.status(401).json({ msg: 'Token Inválido!'});
         }
-})
+});
+
+app.get('/users', async(req, res) => {
+    const users = await database.lsit();
+    res.json(users);
+});
+
+app.post('/users', async(req, res) => {
+    const { name, email, password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preencha todos os campos! '});
+    }
+    await database.create({ name, email, password});
+    res.status(201).send();
+});
+
+app.put('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    await database.update(id, user);
+    res.status(204).send();
+});
+
+app.delete('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    await database.delete(id);
+    res.status(204).send();
+});
+
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
+});
