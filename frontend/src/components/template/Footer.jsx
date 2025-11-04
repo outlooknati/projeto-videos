b50361c6d0a4fbe0ed4b9977a645227c0c6d719d
outlooknati <natisales2107@gmail.com>
2025-11-04 09:39:23 -0300

    Add files via upload

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,2 +1,12 @@
 import './Footer.css';
 import React from 'react';
+
+const Footer = () => (
+    <footer className = "footer">
+        <span>
+            Desenvolvido por Natália
+        </span>
+    </footer>
+);
+
+export default Footer;
