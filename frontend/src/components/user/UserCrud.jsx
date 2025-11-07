e04dd677b3bbfbd183803fc8c4aac06149bfb5a5
outlooknati <natisales2107@gmail.com>
2025-11-07 12:26:28 -0300

    Add files via upload

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -15,3 +15,65 @@ const initialState = {
     user: {name: '', email: ''},
     list: []
 };
+
+export default class UserCrud extends Component{
+    state = {...initialState};
+
+    componentDidMount(){
+        axios(baseUrl)
+            .then(resp => this.setState({list: resp.data}))
+            .catch(err => console.error("Erro ao carregar usuários: " , err));
+    }
+
+
+clear(){
+    this.setState({user: initialState.user});
+}
+
+save(){
+    const user = this.state.user;
+    const method = user.id ? 'put' :'post';
+    const url = user.id ? `${baseUrl}/${user.id} `: baseUrl;
+
+    axios[method](url, user)
+        .then(resp => {
+            const list = this.getUpdateList(resp.data);
+            this.setState({user: initialState.user, list});
+        })
+        .catch(err => console.error("Eroo ao salvar usuário: ", err));
+}
+
+getUpdateList(){
+    const list = this.state.list.filter(u => u.id !== user.id);
+    if(add) list.unshift(user);
+    return list;
+}
+
+getUpdateField(event){
+    const user = {...this.state.user};
+    user[event.target.name] = event.tagert.value;
+    this.setState({user});
+}
+
+renderForm(){
+    return(
+        <div className="form card shadow-sm p-3">
+            <div className="row">
+                <div className="col-12 col-md-12">
+                    <div className="form-group">
+                        <label>Nome</label>
+                        <input
+                            type="text"
+                            name='name'
+                            className="form-control"
+                            value={this.state.user.name}
+                            onChange={e => this.getUpdateField(e)}
+                            placeholder="Digite o nome"
+                        ></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
